ac0c71b534b7a3a6b045ee9578e058f4eaee1929
Gu Qiang <GuQiangJS@users.noreply.github.com>
2018-11-29 17:21:34 +0800

    setup

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+import versioneer
+
+NAME = 'py_translator'
+
+
+def readme():
+    with open('README_GQ.md', encoding='utf-8') as f:
+        return f.read()
+
+
+setup(
+    name=NAME,
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description=readme(),
+    long_description=readme(),
+    author='GuQiangJS',
+    author_email='guqiangjs@gmail.com',
+    url='https://github.com/GuQiangJS/py_translator.git',
+    keywords='translator',
+    install_requires=['requests'],
+    packages=find_packages(),
+    zip_safe=False,
+)
